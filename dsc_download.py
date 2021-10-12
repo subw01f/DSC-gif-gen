6965c322702cb1beab168837e64155a2eea178b7
subw01f <jay@somatic.dev>
2021-10-12 16:58:47 +0000

    adjusted time and added cron

--- dsc_download.py
+++ dsc_download.py
@@ -6,21 +6,21 @@ from PIL import Image
 import os
 
 #filepaths
-fp_in="/root/DSCCam*.jpg"
-fp_out="/root/dsc_loop/loop.gif"
+fp_in="./DSCCam*.jpg"
+fp_out="./DSC_loop.gif"
 image_url = 'https://remote.dsc.org.au/camera/DSCCam.jpg'
 
 for i in range(8):
 	image_filename = wget.download(image_url)
 	print('Image Successfully Downloaded: ', image_filename)
-	time.sleep(360)
+	time.sleep(60)
 
 img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
 img.save(fp=fp_out, format='GIF', append_images=imgs,
          save_all=True, duration=200, loop=0)
 
-os.remove("/root/DSCCam.jpg")
+os.remove("./DSCCam.jpg")
 
 for i in range(1,8):
-	path="/root/DSCCam (%d).jpg" % (i)
+	path="./DSCCam (%d).jpg" % (i)
 	os.remove(path)
